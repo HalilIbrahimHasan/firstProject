0c9504c8c6e3348cba84e9f7d20b482ea57b2d63
Ibrahim Settar <hisetkal@gmail.com>
2020-08-09 11:02:10 -0400

    first commit

---
+++ src/firstPackage/Greetings.java
@@ -0,0 +1,22 @@
+package firstPackage;
+
+public class Greetings{
+	public static void main(String[]args){
+		String a = "Good Morning! kjsdjsandlk";
+		
+		String b = "Good Morning, How are you?";
+
+		String c = "Iam fine. Have a good day!";
+		
+		System.out.println(a+" "+b+" "+c);
+
+		
+
+
+
+//
+//	System.out.println("Good Morning!");
+//	System.out.println("Good Morning, How are you?");
+//	System.out.println("Iam fine. Have a good day!");
+	}
+}
